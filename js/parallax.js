2c94f1d16ba9e13e8e699bad0b7d7df95b2cc6ac
ell-ska <ella@skagerstrom.se>
2023-01-27 14:35:24 +0100

    add message about bug

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,6 @@
 // https://dixonandmoe.com/rellax/#GettingStarted
 // There are som problems with this plugin, especially on mobile
 // I haven't figured out how to fix it yet. For now, just reload in case of issues
-// I'm planing on recreating this in React soon and will find a solution then
+// I'm planing on recreating this in React soon and will find a solution then 
 
 let rellax = new Rellax('.rellax');
